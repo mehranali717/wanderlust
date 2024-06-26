d49574692c24a3fe296d6d88b808cbd38fa8f2ea
Mehran Ali Tehran <Mehranalee110@gmail.com>
2024-06-26 12:45:01 -0700

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route } from "./contextapi/route"
 import routes from "./utils/routes/routes"
+import RouteRenderer from "./views/routeRenderer/routeRenderer"
 const App = () => <Route.Provider value={{routes}}>
+    <RouteRenderer />
 </Route.Provider>
 
 export default App
---
+++ src/views/routeRenderer/routeRenderer.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Route as contextRoute } from '../../contextapi/route';
+import { Route, Routes } from 'react-router-dom';
+
+const RouteRenderer = () => {
+  const { routes } = useContext(contextRoute);
+
+  return (
+      <Routes>
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            element={<route.component />}
+            exact={route.exact}
+          />
+        ))}
+      </Routes>
+  );
+};
+
+export default RouteRenderer;
