bd50cc61002faff0e3679432f3c58cfdc83a52d0
Mehran Ali Tehran <Mehranalee110@gmail.com>
2024-07-01 12:52:19 -0700

    ui fixes

--- src/views/layouts/mainlayout/mainlayout.jsx
+++ src/views/layouts/mainlayout/mainlayout.jsx
@@ -1,10 +1,12 @@
 import Header from "../../sidebar/sidebar";
 import RouteRenderer from "../../routeRenderer/routeRenderer";
-
+import styles from "./mainlayout.module.css";
 const MainLayout = () => (
-  <>
+  <div className={styles.wrapper}>
   <Header />
+  <div className={styles.contentWrapper}>
   <RouteRenderer />
-  </>
+  </div>
+  </div>
 );
 export default MainLayout
--- src/views/navbar/navbar.jsx
+++ src/views/navbar/navbar.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import navbarData from "../../locals/fakeData.json";
+import styles from "./navbar.module.css"
 const Navbar = () => (
-  <nav>
+  <nav className={styles.navbar}>
     <ul>
       {navbarData.navbarItems.map((item) => (
-        <Link to={item.url}><li key={item.id}>{item.navtitle}</li></Link>
+        <Link to={item.url}><li key={item.id} className={styles.listItem}>{item.navtitle}</li></Link>
       ))}
     </ul>
   </nav>
